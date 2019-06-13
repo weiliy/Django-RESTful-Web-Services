d7e4b3f558047f570a42d46280e7ca5742960b7d
Weili Yi <yi@weiliy.net>
2019-06-13 09:25:17 +0800

    carete app

--- Chapter01/hillar_django_restful_01/my_restful01/my_restful01/settings.py
+++ Chapter01/hillar_django_restful_01/my_restful01/my_restful01/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Django REST framework
+    'rest_framwork',
+    # Toys appliation
+    'toys.apps.ToysConfig',
 ]
 
 MIDDLEWARE = [
