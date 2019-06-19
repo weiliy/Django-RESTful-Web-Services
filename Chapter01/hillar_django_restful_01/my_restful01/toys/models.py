de249d0c16323d0b030c6441288f50c2ce924d3a
Weili Yi <yi@weiliy.net>
2019-06-20 02:03:51 +0800

    typo

--- Chapter01/hillar_django_restful_01/my_restful01/toys/models.py
+++ Chapter01/hillar_django_restful_01/my_restful01/toys/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Toy(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_lenght=150, blank=False, default='')
+    name = models.CharField(max_length=150, blank=False, default='')
     description = models.CharField(max_length=250, blank=True, default='')
     toy_category = models.CharField(max_length=200, blank=False, default='')
     release_date = models.DateTimeField()
