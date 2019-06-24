73f3fe05945e944b5e1d5fb31a6445bb4c3611b9
Weili Yi <yi@weiliy.net>
2019-06-25 02:43:34 +0800

    typo

--- my_restful01/toys/views.py
+++ my_restful01/toys/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renders import JSONRender
+from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from rest_framework import status
 from toys.models import Toy
@@ -9,7 +9,7 @@ from toys.serializers import ToySerializer
 
 class JSONResponse(HttpResponse):
     def __init__(self, data, **kwargs):
-        content = JSONRender().render(data)
+        content = JSONRenderer().render(data)
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content, **kwargs)
 
