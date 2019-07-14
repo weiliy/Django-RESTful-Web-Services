0b26a84ec8005c6a2cef61671b6d009f0780a9cc
Weili Yi <yi@weiliy.net>
2019-07-14 16:40:25 +0800

    CH07 defining unique constraints

--- my_restful01/drones/models.py
+++ my_restful01/drones/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class DroneCategory(models.Model):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, unique=True)
 
     class Meta:
         ordering = ('name',)
@@ -11,7 +11,7 @@ class DroneCategory(models.Model):
         return self.name
 
 class Drone(models.Model):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, unique=True)
     drone_category = models.ForeignKey(
         DroneCategory,
         related_name='drones',
@@ -33,7 +33,7 @@ class Pilot(models.Model):
         (MALE, 'Male'),
         (FEMALE, 'Female'),
     )
-    name = models.CharField(max_length=150, blank=False, default='')
+    name = models.CharField(max_length=150, blank=False, default='', unique=True)
     gender = models.CharField(
         max_length=2,
         choices=GENDER_CHOICES,
